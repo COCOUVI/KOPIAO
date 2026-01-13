7efc19b920f28d6b1e9a4d5a7797770d4b755bab
COCOUVI ALEXANDRO <cocouvialexandro74@gmail.com>
2026-01-13 19:55:43 +0100

    Force HTTPS en production via middleware

---
+++ app/Http/Kernel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        // ...existing middleware...
+        \App\Http\Middleware\ForceHttps::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            // ...existing middleware...
+        ],
+
+        'api' => [
+            // ...existing middleware...
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        // ...existing middleware...
+    ];
+}
